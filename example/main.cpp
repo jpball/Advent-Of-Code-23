021b8f75f8b6b2c16f472e7c1e9fd9995a21b5b8
Jordan B. <balljordan28@gmail.com>
2024-11-03 11:59:59 -0600

    Overhaul to template code

--- example/main.cpp
+++ example/main.cpp
@@ -21,64 +21,55 @@ std::vector<std::string> ReadFile(const std::string& filePath);
 
 int main(int argc, char** argv)
 {
-    if(sizeof(TEST_FILE_PATH) == 0 || sizeof(FULL_FILE_PATH) == 0)
-    {
-        std::cout << "-- Must provide file name(s)" << std::endl;
-        return -1;
-    }
-    /*
-    ======
-    Part 1
-    ======
-    */
-    // Evaluating test file
-    std::cout << "----------- EvalOne: TEST -----------" << std::endl;
-    const std::vector<std::string> testData = ReadFile(TEST_FILE_PATH);
-
     if(DEBUG_MODE)
     {
+        std::cout << "-------------- DEBUG MODE -------------" << std::endl;
+        const std::vector<std::string> testData = ReadFile(TEST_FILE_PATH);
+
+        // ======
+        // Part 1
+        // ======
+        std::cout << "----------- EvalOne: TEST -----------" << std::endl;
         std:: cout << "Read line(s): " << testData.size() << std::endl;
+        int testValueOne = EvalOne(testData);
+        std::cout << "Expecting: " << TEST_FILE_EXP_VALUE_ONE << " Actual: " << testValueOne << std::endl;
+        if(TEST_FILE_EXP_VALUE_ONE != testValueOne)
+        {
+            std::cout << "*** P1 TEST FAILURE ***" << std::endl;
+        }
+        // ======
+        // Part 2
+        // ======
+        std::cout << "----------- EvalTwo: TEST -----------" << std::endl;
+        std:: cout << "Read line(s): " << testData.size() << std::endl;
+        int testValueTwo = EvalTwo(testData);
+        std::cout << "Expecting: " << TEST_FILE_EXP_VALUE_TWO << " Actual: " << testValueTwo << std::endl;
+        if(TEST_FILE_EXP_VALUE_TWO != testValueTwo)
+        {
+            std::cout << "*** P2 TEST FAILURE ***" << std::endl;
+        }
     }
-
-    int testValueOne = EvalOne(testData);
-    std::cout << "Expecting: " << TEST_FILE_EXP_VALUE_ONE << " Actual: " << testValueOne << std::endl;
-    if(TEST_FILE_EXP_VALUE_ONE != testValueOne)
+    else
     {
-        std::cout << "Test One failed.... exiting." << std::endl;
-        return -2;
-    }
+        const std::vector<std::string> realData = ReadFile(FULL_FILE_PATH);
 
-    //--
-    // Evaluating real file
-    std::cout << "----------- EvalOne: REAL -----------" << std::endl;
-    const std::vector<std::string> realData = ReadFile(FULL_FILE_PATH);
-
-    if(DEBUG_MODE)
-    {
+        // ======
+        // Part 1
+        // ======
+        std::cout << "----------- EvalOne: REAL -----------" << std::endl;
         std:: cout << "Read line(s): " << realData.size() << std::endl;
+        int realValueOne = EvalOne(realData);
+        std::cout << "Real file result: " << realValueOne << std::endl;
+
+        // ======
+        // Part 2
+        // ======
+        std::cout << "----------- EvalTwo: REAL -----------" << std::endl;
+        std:: cout << "Read line(s): " << realData.size() << std::endl;
+        int realValueTwo = EvalTwo(realData);
+        std::cout << "Real file result: " << realValueTwo << std::endl;
     }
-    int realValueOne = EvalOne(realData);
-    std::cout << "Real file result: " << realValueOne << std::endl;
-
-    /*
-    ======
-    Part 2
-    ======
-    */
-    /*
-    // Evaluating test file for Part 2
-    std::cout << "----------- EvalTwo: TEST -----------" << std::endl;
-    std:: cout << "Read line(s): " << testData.size() << std::endl;
-    int testValueTwo = EvalTwo(testData);
-    std::cout << "Expecting: " << TEST_FILE_EXP_VALUE << " Actual: " << testValueTwo << std::endl;
-
-    //--
-    // Evaluating real file for Part 2
-    std::cout << "----------- EvalTwo: REAL -----------" << std::endl;
-    std:: cout << "Read line(s): " << realData.size() << std::endl;
-    int realValueTwo = EvalTwo(realData);
-    std::cout << "Real file result: " << realValueTwo << std::endl;
-    */
+    
     std::cout << "-------------------------------------" << std::endl;
     return 0;
 }
